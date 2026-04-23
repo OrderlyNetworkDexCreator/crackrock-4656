011bfacd86e8062c74a0c7949e90b0e50723c77d
Mario Reder <mario.reder@pm.me>
2026-04-23 19:52:07 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Swap,Portfolio,Markets",
-  "VITE_CUSTOM_MENUS": "Home, https://crackrockdex.com/",
+  "VITE_CUSTOM_MENUS": "Home, https://crackrockdex.com/; Telegram, https://t.me/CrackrockDex; X (Twitter), https://x.com/CrackRockDEX",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/CrackrockDex",
